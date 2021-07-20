c1a6c27322183843097e13d76fbc70f194b397cb
Amanda Melo <amandamelo86@gmail.com>
2021-07-20 05:46:22 -0300

    Commit inicial do projeto

---
+++ pack-front-end/pack/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>E Lá vamos nós...</h1>
+        <h1>Simbora time!!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
