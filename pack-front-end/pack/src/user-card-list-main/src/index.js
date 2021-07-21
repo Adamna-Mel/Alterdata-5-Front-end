3db8b73fe2aa981fea691d55426997886d2e84ac
zsazsaspeck <giovannemenezeslopes@hotmail.com>
2021-07-21 13:08:49 -0300

    Nav [Faltando ajuste do menu]

--- pack-front-end/pack/src/user-card-list-main/src/index.js
+++ pack-front-end/pack/src/user-card-list-main/src/index.js
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import Nav from "./components/Nav";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <Nav />
     <App />
   </StrictMode>,
   rootElement
