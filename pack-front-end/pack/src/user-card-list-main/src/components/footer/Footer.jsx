9603305528c3a7603de239c84cc5fc969488c7dd
zsazsaspeck <giovannemenezeslopes@hotmail.com>
2021-07-21 17:50:46 -0300

    changes on footer

--- pack-front-end/pack/src/user-card-list-main/src/components/footer/Footer.jsx
+++ pack-front-end/pack/src/user-card-list-main/src/components/footer/Footer.jsx
@@ -8,9 +8,9 @@ class Footer extends Component {
     render(){
         return(
             <div className="main-footer">
-                <div className="col">
+                <div>
                   <img src={alterdata} style={{width:250, height:60, marginLeft:"30%"}}/>
-                  <img src={serratec} style={{width:220, height:47}}/>
+                  <img src={serratec} style={{width:220, height:40, marginBottom:11}}/>
                 </div>
               </div>
             );
