05276663dc4f8a297c1c6de9bbd4af234b732d0a
miqito <81453538+miqito@users.noreply.github.com>
2021-07-28 16:38:33 -0300

    Titulo do time

--- src/components/Team/Create/Create.jsx
+++ src/components/Team/Create/Create.jsx
@@ -1,33 +1,44 @@
 import React from "react";
 
+//MATERIAL-UI
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+//SERVICES
 import apiEquipes from "../../../services/api.equipes";
 
 function Create() {
-	const [nome, SetNome] = React.useState("");
+  const [nome, SetNome] = React.useState("");
 
-	const idUsuario = localStorage.getItem("@user-id");
+  const idUsuario = localStorage.getItem("@user-id");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		const novaEquipe = {
-			nome,
-		};
+    const novaEquipe = {
+      nome,
+    };
 
-		apiEquipes.adicionarEquipe(novaEquipe);
-	};
+    apiEquipes.adicionarEquipe(novaEquipe);
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				type="text"
-				placeholder="Nome do time..."
-				value={nome}
-				onChange={(e) => SetNome(e.target.value)}
-			/>
-			<button type="submit">Criar</button>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        required
+        id="filled-required"
+        label="Nome da Equipe"
+        defaultValue=""
+        variant="filled"
+        onChange={(e) => SetNome(e.target.value)}
+        value={nome}
+        type="text"
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Criar
+      </Button>
+    </form>
+  );
 }
 
 export default Create;
--- src/pages/ListaDeUsuarios.jsx
+++ src/pages/ListaDeUsuarios.jsx
@@ -3,9 +3,18 @@ import { useHistory } from "react-router-dom";
 
 //MATERIAL-UI
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+//SVGColor
+import SvgColor from "react-svg-color";
+
+//ASSETS
+//icons
+import Ninja from "../assets/icons/ninja.svg";
 
 //COMPONENTS
 import UserCard from "../components/UserCard/UserCard";
@@ -15,101 +24,122 @@ import apiUsuarios from "../services/api.usuarios";
 import apiEquipes from "../services/api.equipes";
 
 function ListaDeUsuarios() {
-	const idUsuario = localStorage.getItem("@user-id");
+  const idUsuario = localStorage.getItem("@user-id");
 
-	const [idEquipe, setIdEquipe] = useState("");
-	const [usuarios, setUsuarios] = useState([]);
-	const [anchorEl, setAnchorEl] = useState(null);
-	const history = useHistory();
+  const [idEquipe, setIdEquipe] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const history = useHistory();
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	useEffect(() => {
-		console.log(idUsuario);
-		apiUsuarios
-			.obterUsuarioPorId(idUsuario)
-			.then((res) => {
-				setIdEquipe(res.equipe.idEquipe);
-				apiEquipes.obterEquipesPorId(res.equipe.idEquipe).then((res) => {
-					setUsuarios(res.membros);
-				});
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	}, []);
+  useEffect(() => {
+    console.log(idUsuario);
+    apiUsuarios
+      .obterUsuarioPorId(idUsuario)
+      .then((res) => {
+        setIdEquipe(res.equipe.idEquipe);
+        apiEquipes.obterEquipesPorId(res.equipe.idEquipe).then((res) => {
+          setUsuarios(res.membros);
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
-	const classes = useStyles();
-	return (
-		<>
-			<div>
-				<Button
-					aria-controls="simple-menu"
-					aria-haspopup="true"
-					onClick={handleClick}
-				>
-					Opções do equipe
-				</Button>
-				<Menu
-					id="simple-menu"
-					anchorEl={anchorEl}
-					keepMounted
-					open={Boolean(anchorEl)}
-					onClose={handleClose}
-				>
-					<MenuItem
-						onClick={() => {
-							handleClose;
-							history.push("editar-equipe", idEquipe);
-						}}
-					>
-						Editar
-					</MenuItem>
-					<MenuItem
-						onClick={() => {
-							handleClose;
-							history.push("apagar-equipe", idEquipe);
-						}}
-					>
-						Apagar
-					</MenuItem>
-				</Menu>
-			</div>
-			<div className={classes.card}>
-				{usuarios != undefined ? (
-					usuarios.map((usuario) => (
-						<UserCard
-							key={usuario.id}
-							id={usuario.id}
-							name={usuario.nome}
-							status={usuario.status}
-							role={usuario.cargo.nome}
-							avatar={usuario.avatar}
-						/>
-					))
-				) : (
-					<h1>Time Vazio</h1>
-				)}
-			</div>
-		</>
-	);
+  const classes = useStyles();
+  return (
+    <>
+      <div>
+        <div className={classes.header}>
+          <SvgColor svg={Ninja} width={90} colors={["#000000", "#0083C1"]} />
+          <Typography className={classes.teamName}>
+            COLOCAR NOME DO TIME AQUI
+          </Typography>
+          <IconButton
+            aria-label="more"
+            aria-controls="long-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              handleClose;
+              history.push("editar-equipe", idEquipe);
+            }}
+          >
+            Editar
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleClose;
+              history.push("apagar-equipe", idEquipe);
+            }}
+          >
+            Apagar
+          </MenuItem>
+        </Menu>
+      </div>
+      <div className={classes.card}>
+        {usuarios != undefined ? (
+          usuarios.map((usuario) => (
+            <UserCard
+              key={usuario.id}
+              id={usuario.id}
+              name={usuario.nome}
+              status={usuario.status}
+              role={usuario.cargo.nome}
+              avatar={usuario.avatar}
+            />
+          ))
+        ) : (
+          <h1>Time Vazio</h1>
+        )}
+      </div>
+    </>
+  );
 }
 
 const useStyles = makeStyles({
-	card: {
-		display: "flex",
-		flexDirection: "row",
-		flexWrap: "wrap",
-		justifyContent: "center",
-		alignItems: "center",
-		margin: "auto",
-	},
+  card: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto",
+  },
+  header: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: 400,
+    marginTop: 15,
+    margin: "auto",
+  },
+  teamName: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
 
 export default ListaDeUsuarios;
