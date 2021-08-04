6c90a833f294d5cb7df9c460d0c370d99d7537c8
miqito <81453538+miqito@users.noreply.github.com>
2021-08-04 10:30:25 -0300

    403 e 404 atualizadas

--- src/pages/Forbidden.jsx
+++ src/pages/Forbidden.jsx
@@ -2,10 +2,20 @@ import React from "react";
 
 import forbidden from "../assets/forbidden.png";
 
+import { useHistory } from "react-router-dom";
+
 import useWindowDimensions from "../hooks/WindowDimension";
 
 function Forbidden() {
   const { height, width } = useWindowDimensions();
+
+  const history = useHistory();
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      history.push("/");
+    }, 4000);
+  }, []);
   return (
     <div style={{ minHeight: height }}>
       <img style={{ display: "block", margin: "auto" }} src={forbidden} />
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 import notfound from "../assets/notfound.png";
 
+import { useHistory } from "react-router-dom";
+
 import useWindowDimensions from "../hooks/WindowDimension";
 
 function NotFound() {
   const { height, width } = useWindowDimensions();
+  const history = useHistory();
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      history.push("/");
+    }, 4000);
+  }, []);
   return (
     <div style={{ minHeight: height }}>
       <img style={{ display: "block", margin: "auto" }} src={notfound} />
