2d52f0a8781dbdd94da2f511da262714a6dcbdb1
lopsluan <luanlops1@gmail.com>
2021-08-05 12:49:47 -0300

    Imagem de cargo opcional

--- src/components/Cargos/Create/Create.jsx
+++ src/components/Cargos/Create/Create.jsx
@@ -5,16 +5,16 @@ import apiUsuarios from "../../../services/api.usuarios";
 
 //TODO: Alterar o alerta
 import {
-  Typography,
-  Button,
-  Fade,
-  Backdrop,
-  Modal,
-  makeStyles,
-  TextField,
-  Input,
-  Paper,
-  Card,
+	Typography,
+	Button,
+	Fade,
+	Backdrop,
+	Modal,
+	makeStyles,
+	TextField,
+	Input,
+	Paper,
+	Card,
 } from "@material-ui/core";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
@@ -32,235 +32,243 @@ import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 import { UserContext } from "../../../context/UserContext";
 
 function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
+	return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
 //TODO: tirar o ALERT
 
 function Create({
-  handleClose,
-  api,
-  handleOpenExistentes,
-  contextApi,
-  apiUsuario,
+	handleClose,
+	api,
+	handleOpenExistentes,
+	contextApi,
+	apiUsuario,
 }) {
-  const context = useContext(UserContext);
-  const [onChecked, setOnChecked] = useState(true);
-  const [checked, setChecked] = useState(true);
-  const [nome, setNome] = useState("");
-  const [imagem, setImagem] = useState(null);
-  const [caminho, setCaminho] = useState(null);
-  const [usuario, setUsuario] = useState("");
-  const [msg, setMsg] = useState("");
+	const context = useContext(UserContext);
+	const [onChecked, setOnChecked] = useState(true);
+	const [checked, setChecked] = useState(true);
+	const [nome, setNome] = useState("");
+	const [imagem, setImagem] = useState(null);
+	const [caminho, setCaminho] = useState(null);
+	const [usuario, setUsuario] = useState("");
+	const [msg, setMsg] = useState("");
 
-  useEffect(() => {
-    apiUsuarios
-      .obterUsuarioPorId(context.usuarioAtual)
-      .then((res) => setUsuario(res.nome));
-  }, []);
+	useEffect(() => {
+		apiUsuarios
+			.obterUsuarioPorId(context.usuarioAtual)
+			.then((res) => setUsuario(res.nome));
+	}, []);
 
-  const handleChange = (e) => {
-    onChecked ? setOnChecked(false) : setOnChecked(true);
-    setChecked(e.target.checked);
-  };
+	const handleChange = (e) => {
+		onChecked ? setOnChecked(false) : setOnChecked(true);
+		setChecked(e.target.checked);
+	};
 
-  const handleCriate = () => {
-    const formData = new FormData();
+	const handleCriate = () => {
+		const formData = new FormData();
 
-    formData.append("nome", nome.trim());
-    formData.append("img", imagem);
+		formData.append("nome", nome.trim());
 
-    if (nome.length > 0 && imagem !== null) {
-      apiCargos.adicionarCargo(formData).then((res) => {
-        if (res.status === 201) {
-          setOpenAlert(true);
-          if (checked) {
-            apiUsuarios.editarPapel(context.usuarioAtual, res.data.idCargo);
-            setMsg(" e atribuído com sucesso");
-            contextApi();
-            handleOpenExistentes();
-          }
-        } else {
-          setOpenAlertError(true);
-        }
-      });
-      api();
-      apiUsuario();
-    } else {
-      alert("Algo deu errado!");
-    }
-  };
+		if (nome.length > 0) {
+			apiCargos.adicionarCargo(formData).then((res) => {
+				if (res.status === 201) {
+					setOpenAlert(true);
+					if (checked) {
+						apiUsuarios
+							.editarPapel(context.usuarioAtual, res.data.idCargo)
+							.then((res) => {});
+						setMsg(" e atribuído com sucesso");
+						contextApi();
+						handleOpenExistentes();
+					}
 
-  const handleFile = (e) => {
-    setImagem(e.target.files[0]);
-    setCaminho(URL.createObjectURL(e.target.files[0]));
-  };
+					if (imagem !== null) {
+						formData.append("img", imagem);
+						apiCargos
+							.alterarAvatar(res.data.idCargo, formData)
+							.then((res) => console.log(res));
+					}
+				} else {
+					setOpenAlertError(true);
+				}
+			});
+			api();
+			apiUsuario();
+		} else {
+			alert("Algo deu errado!");
+		}
+	};
 
-  //Snackbar/Alert
-  //AlertSucess
+	const handleFile = (e) => {
+		setImagem(e.target.files[0]);
+		setCaminho(URL.createObjectURL(e.target.files[0]));
+	};
 
-  const [openAlert, setOpenAlert] = React.useState(false);
+	//Snackbar/Alert
+	//AlertSucess
 
-  const handleClickAlert = () => {
-    setOpenAlert(true);
-  };
+	const [openAlert, setOpenAlert] = React.useState(false);
 
-  const handleCloseAlert = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
+	const handleClickAlert = () => {
+		setOpenAlert(true);
+	};
 
-    setOpenAlert(false);
-  };
+	const handleCloseAlert = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
 
-  //
+		setOpenAlert(false);
+	};
 
-  //Alert Error
+	//
 
-  const [openAlertError, setOpenAlertError] = React.useState(false);
+	//Alert Error
 
-  const handleClickAlertError = () => {
-    setOpenAlertError(true);
-  };
+	const [openAlertError, setOpenAlertError] = React.useState(false);
 
-  const handleCloseAlertError = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
+	const handleClickAlertError = () => {
+		setOpenAlertError(true);
+	};
 
-    setOpenAlertError(false);
-  };
+	const handleCloseAlertError = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
 
-  //
+		setOpenAlertError(false);
+	};
 
-  const classes = useStyles();
-  return (
-    <Card elevation={0} className={classes.paper}>
-      <Snackbar
-        open={openAlert}
-        autoHideDuration={3000}
-        onClose={handleCloseAlert}
-      >
-        <Alert onClose={handleCloseAlert} severity="success">
-          Cargo criado com sucesso{msg}!!!
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        open={openAlertError}
-        autoHideDuration={4000}
-        onClose={handleCloseAlertError}
-      >
-        <Alert onClose={handleCloseAlertError} severity="error">
-          Houve algum erro ao criar cargo.
-        </Alert>
-      </Snackbar>
-      <div
-        className={classes.paper}
-        style={{
-          alignSelf: "center",
-          flexDirection: "column",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div style={{ marginLeft: "auto", marginRight: "auto" }}>
-          <img
-            src={caminho}
-            style={{
-              width: 100,
-              height: 100,
-              borderRadius: 400 / 2,
-              borderStyle: "solid",
-              borderColor: "#0083C1",
-              borderWidth: 5,
-              backgroundColor: "#F5F3F4",
-            }}
-          />
-        </div>
-        <Input type="file" onChange={handleFile} />
-        <TextField
-          required
-          id="filled-required"
-          label="Nome (MAX. 14)"
-          defaultValue=""
-          variant="filled"
-          onChange={(e) => setNome(e.target.value)}
-          value={nome}
-          type="text"
-          inputProps={{ maxLength: 14 }}
-          style={{ width: "auto" }}
-          className={classes.inputText}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={onChecked}
-              onChange={handleChange}
-              name="checkedB"
-              color="primary"
-            />
-          }
-          label={`Atribuir cargo a ${usuario}.`}
-        />
-        <div>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={handleCriate}
-          >
-            Criar
-          </Button>
-        </div>
-        <div>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={handleOpenExistentes}
-            className={classes.button}
-          >
-            Cancelar
-          </Button>
-        </div>
-      </div>
-    </Card>
-  );
+	//
+
+	const classes = useStyles();
+	return (
+		<Card elevation={0} className={classes.paper}>
+			<Snackbar
+				open={openAlert}
+				autoHideDuration={3000}
+				onClose={handleCloseAlert}
+			>
+				<Alert onClose={handleCloseAlert} severity="success">
+					Cargo criado com sucesso{msg}!!!
+				</Alert>
+			</Snackbar>
+			<Snackbar
+				open={openAlertError}
+				autoHideDuration={4000}
+				onClose={handleCloseAlertError}
+			>
+				<Alert onClose={handleCloseAlertError} severity="error">
+					Houve algum erro ao criar cargo.
+				</Alert>
+			</Snackbar>
+			<div
+				className={classes.paper}
+				style={{
+					alignSelf: "center",
+					flexDirection: "column",
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<div style={{ marginLeft: "auto", marginRight: "auto" }}>
+					<img
+						src={caminho}
+						style={{
+							width: 100,
+							height: 100,
+							borderRadius: 400 / 2,
+							borderStyle: "solid",
+							borderColor: "#0083C1",
+							borderWidth: 5,
+							backgroundColor: "#F5F3F4",
+						}}
+					/>
+				</div>
+				<Input type="file" onChange={handleFile} />
+				<TextField
+					required
+					id="filled-required"
+					label="Nome (MAX. 14)"
+					defaultValue=""
+					variant="filled"
+					onChange={(e) => setNome(e.target.value)}
+					value={nome}
+					type="text"
+					inputProps={{ maxLength: 14 }}
+					style={{ width: "auto" }}
+					className={classes.inputText}
+				/>
+				<FormControlLabel
+					control={
+						<Checkbox
+							checked={onChecked}
+							onChange={handleChange}
+							name="checkedB"
+							color="primary"
+						/>
+					}
+					label={`Atribuir cargo a ${usuario}.`}
+				/>
+				<div>
+					<Button
+						variant="contained"
+						color="primary"
+						className={classes.button}
+						onClick={handleCriate}
+					>
+						Criar
+					</Button>
+				</div>
+				<div>
+					<Button
+						variant="contained"
+						color="secondary"
+						onClick={handleOpenExistentes}
+						className={classes.button}
+					>
+						Cancelar
+					</Button>
+				</div>
+			</div>
+		</Card>
+	);
 }
 
 export default Create;
 
 const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    borderRadius: 20,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    textAlign: "center",
-  },
-  button: {
-    marginTop: 15,
-  },
-  buttonCancel: {
-    margin: 5,
-    backgroundColor: "#F22",
-    "&:hover": {
-      backgroundColor: "#F00",
-    },
-  },
-  inputText: {
-    marginTop: 7,
-    marginBottom: 7,
-  },
+	modal: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	paper: {
+		borderRadius: 20,
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		textAlign: "center",
+	},
+	button: {
+		marginTop: 15,
+	},
+	buttonCancel: {
+		margin: 5,
+		backgroundColor: "#F22",
+		"&:hover": {
+			backgroundColor: "#F00",
+		},
+	},
+	inputText: {
+		marginTop: 7,
+		marginBottom: 7,
+	},
 }));
 
 const papercss = {
-  padding: "25px 20px",
-  width: 400,
-  margin: "30px auto",
-  borderRadius: 20,
+	padding: "25px 20px",
+	width: 400,
+	margin: "30px auto",
+	borderRadius: 20,
 };
--- src/components/RemoveUser/RemoveUser.jsx
+++ src/components/RemoveUser/RemoveUser.jsx
@@ -1,104 +1,104 @@
-// import React from "react";
-// import { useHistory } from "react-router-dom";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-// import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
-// import {
-// 	Typography,
-// 	Button,
-// 	Fade,
-// 	Backdrop,
-// 	Modal,
-// 	Card,
-// } from "@material-ui/core";
+import {
+	Typography,
+	Button,
+	Fade,
+	Backdrop,
+	Modal,
+	Card,
+} from "@material-ui/core";
 
-// function RemoveUser({ openModal, setOpenModal, idEquipe }) {
-// 	const [equipe, setEquipe] = React.useState("");
-// 	const history = useHistory();
+function RemoveUser({ openModal, setOpenModal, idEquipe }) {
+	const [equipe, setEquipe] = React.useState("");
+	const history = useHistory();
 
-// 	React.useEffect(() => {
-// 		apiEquipe.obterEquipesPorId(idEquipe).then((res) => {
-// 			setEquipe(res.nome);
-// 		});
-// 	}, [openModal]);
+	React.useEffect(() => {
+		apiEquipe.obterEquipesPorId(idEquipe).then((res) => {
+			setEquipe(res.nome);
+		});
+	}, [openModal]);
 
-// 	const classes = useStyles();
+	const classes = useStyles();
 
-// 	const handleClose = () => {
-// 		setOpenModal(false);
-// 	};
+	const handleClose = () => {
+		setOpenModal(false);
+	};
 
-// 	const handleRemove = () => {};
+	const handleRemove = () => {};
 
-// 	return (
-// 		<div>
-// 			<Modal
-// 				aria-labelledby="transition-modal-title"
-// 				aria-describedby="transition-modal-description"
-// 				className={classes.modal}
-// 				open={openModal}
-// 				onClose={handleClose}
-// 				closeAfterTransition
-// 				BackdropComponent={Backdrop}
-// 				BackdropProps={{
-// 					timeout: 500,
-// 				}}
-// 			>
-// 				<Fade in={openModal}>
-// 					<div
-// 						className={classes.paper}
-// 						style={{
-// 							alignSelf: "center",
-// 							flexDirection: "column",
-// 							display: "flex",
-// 							justifyContent: "center",
-// 						}}
-// 					>
-// 						<Button
-// 							// style={{  }}
-// 							onClick={handleRemove}
-// 							variant="contained"
-// 							color="primary"
-// 							className={classes.buttonDelete}
-// 						>
-// 							Sim
-// 						</Button>
-// 						<Button
-// 							onClick={handleClose}
-// 							variant="contained"
-// 							color="secondary"
-// 							className={classes.button}
-// 						>
-// 							Cancelar
-// 						</Button>
-// 					</div>
-// 				</Fade>
-// 			</Modal>
-// 		</div>
-// 	);
-// }
-// const useStyles = makeStyles((theme) => ({
-// 	modal: {
-// 		display: "flex",
-// 		alignItems: "center",
-// 		justifyContent: "center",
-// 		flexDirection: "column",
-// 	},
-// 	paper: {
-// 		backgroundColor: theme.palette.background.paper,
-// 		boxShadow: theme.shadows[5],
-// 		padding: theme.spacing(2, 4, 3),
-// 		borderRadius: 20,
-// 	},
-// 	button: {
-// 		marginTop: 15,
-// 	},
-// 	buttonDelete: {
-// 		marginTop: 15,
-// 		backgroundColor: "#F22",
-// 		"&:hover": {
-// 			backgroundColor: "#F00",
-// 		},
-// 	},
-// }));
-// export default RemoveUser;
+	return (
+		<div>
+			<Modal
+				aria-labelledby="transition-modal-title"
+				aria-describedby="transition-modal-description"
+				className={classes.modal}
+				open={openModal}
+				onClose={handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}
+			>
+				<Fade in={openModal}>
+					<div
+						className={classes.paper}
+						style={{
+							alignSelf: "center",
+							flexDirection: "column",
+							display: "flex",
+							justifyContent: "center",
+						}}
+					>
+						<Button
+							// style={{  }}
+							onClick={handleRemove}
+							variant="contained"
+							color="primary"
+							className={classes.buttonDelete}
+						>
+							Sim
+						</Button>
+						<Button
+							onClick={handleClose}
+							variant="contained"
+							color="secondary"
+							className={classes.button}
+						>
+							Cancelar
+						</Button>
+					</div>
+				</Fade>
+			</Modal>
+		</div>
+	);
+}
+const useStyles = makeStyles((theme) => ({
+	modal: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "column",
+	},
+	paper: {
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+		borderRadius: 20,
+	},
+	button: {
+		marginTop: 15,
+	},
+	buttonDelete: {
+		marginTop: 15,
+		backgroundColor: "#F22",
+		"&:hover": {
+			backgroundColor: "#F00",
+		},
+	},
+}));
+export default RemoveUser;
