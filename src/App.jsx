dc8cb96bad6ac876b6f986cdbca1307a5fd0194e
lopsluan <luanlops1@gmail.com>
2021-08-05 09:40:02 -0300

    correção de bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,22 @@ import Rotas from "./routes/Rotas";
 
 import UserProvider from "./context/UserContext";
 
-//TODO: Atualizar a senha
-
 //TODO: fazer a imagem padrao de cargo usuario e time se naõ conseguir arrumar as mensagem do cadastro de usuario
 
 //TODO: Tirar o texto double click dos inputs
 
 //TODO: Estilizar o perfil para ficar mais intuitivo
 
-//TODO: consertar o lapizinho
+//TODO: consertar o lapisinho
 
 //TODO: colocar a paginação entre a lista de cargos e o botao
 
-//TODO: Altear a visualização dos inputs no perfil (deixar so os)
+//TODO: Altear a visualização dos inputs no perfil
 
 //TODO: Colocar os modais e recarregar as paginas
 
 //TODO: tirar as 99 pag da lista de usuario
+
 function App() {
 	return (
 		<UserProvider>
